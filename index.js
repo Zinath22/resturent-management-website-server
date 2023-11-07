f4244948d7f69f4c850df5b13e1f941346b459c8
Zinath22 <Zinath22@users.noreply.github.com>
2023-11-07 11:13:35 +0600

    update form added from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -8,19 +9,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-// ZGsn7nDoreYmObrS
+
+
 // resturentManagement
+// 9b6gEpbZRi7Yebhv
 
-// zinathfarhana22
-
-// SNjaSaFFA8zlnG8Q
-
+// console.log(process.env.DB_USER)
 
 
-const uri = "mongodb+srv://zinathfarhana22:SNjaSaFFA8zlnG8Q@cluster0.w2tuwt2.mongodb.net/?retryWrites=true&w=majority";
+
+// last 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w2tuwt2.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -32,7 +41,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const foodCollection = client.db('resturentDB').collection('allFood');
     const purchaseCollection = client.db('resturentDB').collection('purchase');
@@ -76,13 +85,41 @@ async function run() {
 
       })
     
-
+// add 
     app.post('/allFood', async(req, res)=> {
       const newFood = req.body;
       console.log(newFood);
       const result = await foodCollection.insertOne(newFood);
       res.send(result); 
     })
+
+    // update 
+    app.put('/allFood', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true};
+      const updateFood = req.body;
+      const food = {
+        $set: {
+     
+           food_name : updateFood.food_name,
+        food_category : updateFood.food_category,
+           quantity : updateFood.quantity,
+           food_description : updateFood.food_description,
+           price : updateFood.price,
+          added_by_name : updateFood.added_by_name,
+           added_by_email : user?.email, // Make sure user is defined
+           food_img : updateFood.food_img,
+          food_origin : updateFood.food_origin
+        }
+      }
+     const result = await foodCollection.updateOne(filter, food, options);
+     res.send(result);
+    })
+
+
+    
+
 
     //  // addfood 
     //  app.post('/allFood', async(res, res) => {
