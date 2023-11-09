789343869726ce1d1e2232f14fd6395579520726
Zinath22 <Zinath22@users.noreply.github.com>
2023-11-09 23:51:25 +0600

    vercel.json added

--- index.js
+++ index.js
@@ -21,13 +21,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
